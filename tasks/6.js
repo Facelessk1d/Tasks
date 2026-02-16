9d37c9f2a26632c2a2e56ba3fb2a40618d286632
Facelessk1d <damirsokirov50@gmail.com>
2026-02-16 23:33:56 +0400

    ну сегодня с братом он мне пояснял за программирование дай бог ему здоровья

--- tasks/6.js
+++ tasks/6.js
@@ -1,7 +1,41 @@
-let x = prompt()
+// Калькулятор: Запроси у пользователя два числа и операцию (+, -, *, /). 
+//Выведи результат, используя if или switch.
 
-const sumFunc = (operator, number1, number2) => {
-    const promtedOperator = prompt("Введите оператор из [+, -, *, /, %]:");
+const operationFunc = (number1, number2) => {
+    let x;
+    let  uslovie = false
+    while(!uslovie) {
+        const promtedOperator = prompt("Введите оператор из [+, -, *, /,]:");
+        switch (promtedOperator) {
+            case "+":
+                x = number1 + number2
+                uslovie = true
+                break;
+                
+            case "*":
+                x = number1 * number2
+                uslovie = true
+                break;
+                
+            case "/":
+                x = number1 / number2
+                uslovie = true
+                break;
+                
+            case "-":
+                x = number1 - number2
+                uslovie = true
+                break;
+
+            case "%":
+                x = number1 % number2
+                uslovie = true
+                break;
+
+            default:
+                console.log("ты даун ээээ ыыы ээ ")
+        }
+    }
+    return x
 }
-
-
+console.log(operationFunc(3, 4324))
