9ed143a2443e3e78f3d5aab0ccf8725744f3083b
Facelessk1d <damirsokirov50@gmail.com>
2026-02-19 00:41:14 +0400

    T_T...

---
+++ tasks/8.js
@@ -0,0 +1,17 @@
+// Проверка четности: Запроси у пользователя число. Определи и выведи, 
+//является ли число четным (используй оператор % — остаток от деления).
+
+const isEvenFunc = () => {
+    const num = prompt("введите число")
+    const funj = num % 2
+    
+    if (funj == 0) {    
+        alert("число четное");   
+    }
+
+    else {
+        alert("outworld destroyer from dota 2 ")
+    }
+    
+}
+isEvenFunc()
