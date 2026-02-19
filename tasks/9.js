49d4b95f5a92c95128066da9d8414f6189d6cb46
Facelessk1d <damirsokirov50@gmail.com>
2026-02-19 23:11:24 +0400

    девочка эмо

---
+++ tasks/9.js
@@ -0,0 +1,11 @@
+// Счетчик: Используй цикл for, чтобы вывести в консоль числа от 1 до 10, а затем от 10 до 1.
+
+let x = 1
+while(x < 10) {
+  console.log(x++)
+}
+let y = 10
+while(y >=1 ) {
+    console.log(y--)
+}
+console.log(x, y)
